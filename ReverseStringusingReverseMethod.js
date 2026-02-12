f7f47c1aeda3e0899d3aad5c20effe7c92536bf8
ashuqa23 <ashuqa23@gmail.com>
2026-02-12 22:22:41 +0530

    new program

---
+++ ReverseStringusingReverseMethod.js
@@ -0,0 +1,7 @@
+// Declare a string
+let str = "javascript";
+// Reverse a string using builtin methods
+let reversed = str.split("").reverse().join("");
+//Print the result
+console.log("Original string :", str);
+console.log("Reversed string :", reversed);
