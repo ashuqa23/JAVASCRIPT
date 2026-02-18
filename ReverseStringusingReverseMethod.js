718cd6278fd9119885c5ff1cdb3c905586b27edc
ashuqa23 <ashuqa23@gmail.com>
2026-02-18 22:48:13 +0530

    new program

--- ReverseStringusingReverseMethod.js
+++ ReverseStringusingReverseMethod.js
@@ -1,7 +1,24 @@
 // Declare a string
-let str = "javascript";
+/* let str = "javascript";
 // Reverse a string using builtin methods
 let reversed = str.split("").reverse().join("");
 //Print the result
 console.log("Original string :", str);
-console.log("Reversed string :", reversed);
+console.log("Reversed string :", reversed); */
+
+// Import readline module
+const readline = require("readline");
+// Create interface for input and output
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+// Ask user for input
+rl.question("Enter a string", function(input) {
+    // Reverse string using built in method
+    let reversed = input.split("").reverse().join("");
+    // Print the result
+    console.log("Reversed string:", reversed);
+    // Close the interface
+    rl.close();
+});
